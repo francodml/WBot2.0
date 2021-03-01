555f5805a4b21f8a18036568ea99fd1d5a8614ea
Win7yes <francodmeneguzzi@gmail.com>
2021-03-01 01:16:08 -0300

    MemberInfo HasCustomAttribute<T> extension

---
+++ Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace WBot2.Extensions
+{
+    public static class MemberInfoExtensions
+    {
+        public static bool HasCustomAttribute<T> (this MemberInfo info) where T : Attribute
+        {
+            return info.GetCustomAttribute<T>() != null;
+        }
+    }
+}
