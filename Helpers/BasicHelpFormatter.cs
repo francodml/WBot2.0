a1c08b28d3ec960f342a09292f0a12ff78fe878f
Win7yes <francodmeneguzzi@gmail.com>
2021-03-05 18:58:26 -0300

    Basic Help formatter We probably don't need to register it as a service, but it'll do for now.

--- Helpers/BasicHelpFormatter.cs
+++ Helpers/BasicHelpFormatter.cs
@@ -1,14 +1,23 @@
 ﻿using DSharpPlus.Entities;
 using System.Threading.Tasks;
 using WBot2.Helpers.Interfaces;
+using WBot2.Commands;
 
 namespace WBot2.Helpers
 {
-    public class BasicHelpFormatter : IHelpFormatter
+    public class BasicHelpFormatter : IHelpFormatter<DiscordEmbedBuilder>
     {
-        public Task<DiscordEmbed> FormatHelp(ICommandHandler commandHandler)
+        public async Task<DiscordEmbedBuilder> FormatHelp(ICommandHandler commandHandler)
         {
-            throw new System.NotImplementedException();
+            var embed = new DiscordEmbedBuilder()
+                .WithTitle("Comandos")
+                .WithDescription("Esta es la lista de comandos disponibles");
+            foreach (Command command in commandHandler.Commands)
+            {
+                embed.AddField(command.Name, command.Description);
+            }
+            //await e.Message.RespondAsync($"Commands: {string.Join(", ", Commands.Select(x => x.GetCustomAttribute<CommandAttribute>().Name))}");
+            return embed;
         }
     }
 }
