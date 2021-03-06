d8b58cce840af1a5bb83c07ca74987d6b15b5bd4
Win7yes <francodmeneguzzi@gmail.com>
2021-03-06 00:42:35 -0300

    Display commands under their modules

--- Helpers/BasicHelpFormatter.cs
+++ Helpers/BasicHelpFormatter.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus.Entities;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 using WBot2.Helpers.Interfaces;
 using WBot2.Commands;
 
@@ -10,13 +12,16 @@ namespace WBot2.Helpers
         public async Task<DiscordEmbedBuilder> FormatHelp(ICommandHandler commandHandler)
         {
             var embed = new DiscordEmbedBuilder()
-                .WithTitle("Comandos")
-                .WithDescription("Esta es la lista de comandos disponibles");
-            foreach (Command command in commandHandler.Commands)
+                .WithTitle("Commands")
+                .WithDescription("This is a list of available commands, sorted into their modules.");
+
+            foreach (BaseCommandModule baseModule in commandHandler.CommandModules)
             {
-                embed.AddField(command.Name, command.Description);
+                var cmds = commandHandler.Commands.Where(x => x.Module == baseModule);
+                List<string> cmdDescs = cmds.Select(x => $"{x.Name}: {x.Description}\n").ToList();
+                string listString = cmdDescs.Aggregate((a, b) => a + b);
+                embed.AddField(baseModule.ModuleName, listString);
             }
-            //await e.Message.RespondAsync($"Commands: {string.Join(", ", Commands.Select(x => x.GetCustomAttribute<CommandAttribute>().Name))}");
             return embed;
         }
     }
