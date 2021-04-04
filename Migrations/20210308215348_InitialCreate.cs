84ff522e59dc1e5ce3ca92eaf51cf108e028f1a6
Win7yes <francodmeneguzzi@gmail.com>
2021-04-04 20:32:18 -0300

    Merge branch 'dev'

---
+++ Migrations/20210308215348_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WBot2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guilds",
+                columns: table => new
+                {
+                    ID = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guilds", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchedMessages",
+                columns: table => new
+                {
+                    ID = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmojiRolesJS = table.Column<string>(type: "TEXT", nullable: true),
+                    GuildID = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchedMessages", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_WatchedMessages_Guilds_GuildID",
+                        column: x => x.GuildID,
+                        principalTable: "Guilds",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchedMessages_GuildID",
+                table: "WatchedMessages",
+                column: "GuildID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WatchedMessages");
+
+            migrationBuilder.DropTable(
+                name: "Guilds");
+        }
+    }
+}
